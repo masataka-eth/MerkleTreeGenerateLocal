bd30a8e49496453dc24a3fd51a1e678ccb1d6b9c
henma-dev <e_pinanko@yahoo.co.jp>
2022-07-28 15:42:51 +0900

    new

---
+++ README.md
@@ -0,0 +1,29 @@
+# MerkleTree Generate Local for Sha3-256
+
+## Why
+Markle Tree must be created after the developer is responsible for reading the code!
+
+## How to Use
+After clone this repositry:
+```
+npm install
+```
+
+### Reserve
+ - You must prepare your whitelist as a newline delimited list of text
+ - File name should be "whitelist.txt"
+ - Set in ".interface"folder
+
+### Execute
+
+```
+cd yourDirectory
+node index.js
+```
+
+Two files are created in the ".interface" folder
+ - root.txt : MerkleRoot(write your contract)
+ - proof.json : Proof information per wallet (use at mint site) 
+'*proof.json is in a format that can be imported directly into Firestore'
+
+That's all.
---
+++ index.js
@@ -0,0 +1,45 @@
+const { MerkleTree } = require('merkletreejs')
+const keccak256 = require('keccak256')
+const { buf2hex } = require('arrbuf2hex')
+const fs = require('fs');
+
+// read whitelist textdata
+const text = fs.readFileSync(".interface/whitelist.txt", 'utf-8');
+
+// node->byte
+let nodedata = text.split(/\r\n|\n/);
+nodedata = nodedata.filter(Boolean);    // delete space
+const leaves = nodedata.map(x => keccak256(x))
+
+// tree compute
+const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
+const hexroot = buf2hex(tree.getRoot())
+
+// make proof json file
+const LEAD_VAL = '{\"whitelist\": {';
+const LAST_VAL = '}}';
+let listdata = '';
+nodedata.forEach(function(element){
+    const id = element.toLowerCase();
+    const leaf = keccak256(id)
+    const hexproof = tree.getProof(leaf).map(x => buf2hex(x.data))
+    proofdata = "";
+    hexproof.forEach(function(proof){
+        proofdata += '0x'+proof + ','
+    });
+    proofdata = proofdata.slice(0,-1);
+    listdata += '\"' + id + '\"' + ':{\"hexProof\":\"' +  proofdata + '\"},'
+});
+listdata = listdata.slice(0,-1);
+const OutoputJson = LEAD_VAL + listdata + LAST_VAL;
+
+const createFile = (pathName, source) => {
+fs.writeFile(pathName, source, (err) => {
+    if (err) rej(err);
+    if (!err) {
+    console.log('Generated file!');
+    }
+});
+};
+createFile('.interface/proof.json', OutoputJson);
+createFile('.interface/root.txt', '0x' + hexroot);
