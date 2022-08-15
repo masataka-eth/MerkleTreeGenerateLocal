3c5a565d03cc24d003912b2486822bbfcfaab831
henma-dev <e_pinanko@yahoo.co.jp>
2022-08-15 18:16:25 +0900

    miss comment

--- index.js
+++ index.js
@@ -14,34 +14,34 @@ const leaves = nodedata.map(x => keccak256(x))
 console.log(nodedata);
 
 // tree compute
-// const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
-// const hexroot = buf2hex(tree.getRoot())
+const tree = new MerkleTree(leaves, keccak256, { sortPairs: true })
+const hexroot = buf2hex(tree.getRoot())
 
-// // make proof json file
-// const LEAD_VAL = '{\"whitelist\": {';
-// const LAST_VAL = '}}';
-// let listdata = '';
-// nodedata.forEach(function(element){
-//     const id = element.toLowerCase();
-//     const leaf = keccak256(id)
-//     const hexproof = tree.getProof(leaf).map(x => buf2hex(x.data))
-//     proofdata = "";
-//     hexproof.forEach(function(proof){
-//         proofdata += '0x'+proof + ','
-//     });
-//     proofdata = proofdata.slice(0,-1);
-//     listdata += '\"' + id + '\"' + ':{\"hexProof\":\"' +  proofdata + '\"},'
-// });
-// listdata = listdata.slice(0,-1);
-// const OutoputJson = LEAD_VAL + listdata + LAST_VAL;
+// make proof json file
+const LEAD_VAL = '{\"whitelist\": {';
+const LAST_VAL = '}}';
+let listdata = '';
+nodedata.forEach(function(element){
+    const id = element.toLowerCase();
+    const leaf = keccak256(id)
+    const hexproof = tree.getProof(leaf).map(x => buf2hex(x.data))
+    proofdata = "";
+    hexproof.forEach(function(proof){
+        proofdata += '0x'+proof + ','
+    });
+    proofdata = proofdata.slice(0,-1);
+    listdata += '\"' + id + '\"' + ':{\"hexProof\":\"' +  proofdata + '\"},'
+});
+listdata = listdata.slice(0,-1);
+const OutoputJson = LEAD_VAL + listdata + LAST_VAL;
 
-// const createFile = (pathName, source) => {
-// fs.writeFile(pathName, source, (err) => {
-//     if (err) rej(err);
-//     if (!err) {
-//     console.log('Generated file!');
-//     }
-// });
-// };
-// createFile('.interface/proof.json', OutoputJson);
-// createFile('.interface/root.txt', '0x' + hexroot);
+const createFile = (pathName, source) => {
+fs.writeFile(pathName, source, (err) => {
+    if (err) rej(err);
+    if (!err) {
+    console.log('Generated file!');
+    }
+});
+};
+createFile('.interface/proof.json', OutoputJson);
+createFile('.interface/root.txt', '0x' + hexroot);
